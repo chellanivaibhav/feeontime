66440c44eeaf2a084780902829d0810abdc53e96
Rahul Malhotra <rahuldaphnis96@gmail.com>
2017-03-18 10:37:40 +0530

    Navigation Drawer Added

--- src/index.js
+++ src/index.js
@@ -3,15 +3,53 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
-import {AppBar, IconButton, FlatButton} from 'material-ui';
+import {AppBar, Drawer, MenuItem, IconButton, FlatButton, Toolbar} from 'material-ui';
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
+var MyDrawer = React.createClass({
+
+	getInitialState: function()
+	{
+		 return {open: false};
+	},
+
+ 	handleToggle :function() { this.setState({open: !this.state.open}) },
+
+ 	handleClose : function() { this.setState({open: false}) },
+
+	render: function() {
+
+		  var buttonStyle = {
+		    backgroundColor: 'transparent',
+		    color: 'white'
+		  };
+
+		return (
+			<div>
+			  <AppBar title="fee on time"
+			   onLeftIconButtonTouchTap={this.handleToggle}>
+			  </AppBar>
+			  <Drawer
+	           docked={false}
+	           open={this.state.open}
+	           onRequestChange={(open) => this.setState({open})}
+          	  >
+			  <MenuItem onTouchTap={this.handleClose} >Menu Item</MenuItem>
+			  <MenuItem onTouchTap={this.handleClose} >Menu Item 2</MenuItem>
+			  </Drawer>
+			</div>
+
+		);
+	}
+
+});
+
 const MyAwesomeReactComponent = () => (
-  <AppBar title="Fee On Time"
-   iconElementRight={<FlatButton label="Default" />}>
-  </AppBar>
+  <div>
+  <MyDrawer />
+  </div>
 );
 
 const App = () => (
